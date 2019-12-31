4b9b4a0b33d5831b783887d5a1a9d8c4cde36df4
陈益岗 <1154610080@qq.com>
2019-12-31 16:48:11 +0800

    完成力扣算法练习:237. 删除链表中的节点

---
+++ 力扣/算法/237. 删除链表中的节点.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Administrator on 2019/12/31.
+//
+
+**
+* Definition for singly-linked list.
+* struct ListNode {
+    *     int val;
+    *     ListNode *next;
+    *     ListNode(int x) : val(x), next(NULL) {}
+    * };
+*/
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
