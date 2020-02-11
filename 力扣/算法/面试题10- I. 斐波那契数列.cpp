8befbff5db67a4be406ede37c9f3f3aa3383ce45
Egan <1154610080@qq.com>
2020-02-11 22:28:12 +0800

    完成力扣剑指Offer:面试题10-I. 斐波那契数列

---
+++ 力扣/算法/面试题10- I. 斐波那契数列.cpp
@@ -0,0 +1,25 @@
+// begin at 2020-02-11 22:10:02
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	int fib(int n) {
+		int a = 0, b = 1, c = n >= 1 ? a+b : a;
+        for(int i=2, temp; i<=n; i++){
+        	c = (a + b) % 1000000007;
+        	a = b;
+        	b = c;
+		}
+		return c;
+    }
+};
+
+int main(){
+	Solution *s = new Solution();
+	cout << s->fib(7) << endl;
+	return 0;
+}
+
+// finish at 2020-02-11 22:27:49 
