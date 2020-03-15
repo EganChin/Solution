2dd3da0e7ac65b8b2d831f88bab7ae9fa58cb091
Egan <1154610080@qq.com>
2020-03-15 17:49:56 +0800

    5172. 形成三的最大倍数

--- 力扣/竞赛/177周赛/5172. 形成三的最大倍数.java
+++ 力扣/竞赛/177周赛/5172. 形成三的最大倍数.java
@@ -1,35 +1,45 @@
-// begin at 2020/2/23 13:10
+//2020-03-15 17:30:43
 /*
-如果数组和模三为零，则说明可以任意组成三的倍数
-如果余2，去掉一个2或两个1使其模三为零
-如果与1，去掉一个1或两个2使其模三为零
+8+1+9 == 18
+18%3 == 0，任意数组合可模三为零
+8+1+9+2 % 3 == 2, 去掉一个2可模三为零
+8+1+1+1+9 % 3 == 2，去掉两个1可模三为零
+8+1+1+9 % 3 == 1，去掉一个1可模三为零
+2+2+9+9 % 3 == 1，去掉两个2可模三为零
+
+得出结论：
+模三为零时，不需要处理
+模三为2时，去掉两个1或一个2
+模三为1时，去掉两个2或一个1
 */
 class Solution {
-    private int []cnt = new int[10];
-    boolean delete( int x){
-        for(int i=x; i<=9; i+=3)
-            //没有目标数，就跨三删除下一个，结果一致
+    int []cnt = new int[10];
+
+    private boolean delete(int x){
+        //如果目标不存在，跨三删除下一个
+        for(int i=x; i<=9; i+=3){
             if(cnt[i] != 0){
                 cnt[i]--;
                 return true;
             }
+        }
         return false;
     }
+
     public String largestMultipleOfThree(int[] digits) {
         int sum = 0;
-        for(int i : digits){
+        for(int i: digits){
+            sum+=i;
             cnt[i]++;
-            sum += i;
         }
-        if(sum == 0)
-            return "0";
-        if(sum%3 == 2){
+        if(sum == 0) return "0";
+        if(sum%3==2){
             if(!delete(2)){
                 delete(1);
                 delete(1);
             }
         }
-        if(sum%3 == 1){
+        if(sum%3==1){
             if(!delete(1)){
                 delete(2);
                 delete(2);
@@ -37,13 +47,11 @@ class Solution {
         }
         String ans = "";
         for(int i=9; i>=0; i--){
-            while(cnt[i] != 0){
-                ans += i;
-                cnt[i]--;
+            while(cnt[i]-- != 0){
+                ans+=i;
             }
         }
         return ans;
     }
 }
-
-// finish at 2020/2/23 13:41
+//2020-03-15 17:49:25
