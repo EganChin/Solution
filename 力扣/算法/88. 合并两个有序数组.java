03b58d4bbe5fc49dbc02b065baeefe4eefeb7915
Egan <1154610080@qq.com>
2020-03-16 17:46:41 +0800

    88. 合并两个有序数组(review)

--- 力扣/算法/88. 合并两个有序数组.java
+++ 力扣/算法/88. 合并两个有序数组.java
@@ -1,12 +1,12 @@
-// begin at 2020-02-26 23:55:55
+//2020-03-16 17:39:33
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i=m-1, j=n-1, k = m+n-1;
-        while(i>=0 && j>=0){
-            nums1[k--] = (nums1[i]<nums2[j]) ? nums2[j--] : nums1[i--];
+        int i=m-1, j=n-1, cnt=m+n-1;
+        while(i>=0&&j>=0){
+            nums1[cnt--] = nums1[i]>nums2[j] ? nums1[i--]: nums2[j--];
         }
-        System.arraycopy(nums2, 0, nums1, 0, j+1);
+        while(i>=0) nums1[cnt--] = nums1[i--];
+        while(j>=0) nums1[cnt--] = nums2[j--];
     }
 }
-
-//finish at 2020-02-27 00:13:32
+//2020-03-16 17:45:40
