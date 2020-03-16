c81433528db6078be90bc6186afe872ffd66c2c4
Egan <1154610080@qq.com>
2020-03-16 17:34:21 +0800

    1359. 有效的快递序列数目(review)

--- 力扣/剑指Offer/5326. 有效的快递序列数目/Solution.java
+++ 力扣/剑指Offer/5326. 有效的快递序列数目/Solution.java
@@ -1,19 +1,19 @@
-// begin at 2020/2/24 11:11
-/**
- * n = 1，F(1)=1
- * n = 2, F(2)=1*C(2, 4)
- * n = 3, F(3)=1*C(2, 4)*C(2, 6)
- * n = 4, F(4)=1*C(2, 4)*C(2, 6)*C(2, 8)
- * F(n) = F(n-1)*C*(2,2n) = F(n-1)*n*(2*n-1)
- **/
+//2020-03-16 16:51:04
+/*
+F(1) = 1
+F(2) = F(1) * C(2, 4)
+F(3) = F(2) * C(2,6)
+F(n) = F(n-1) * C(2, 2*n)
+     = F(n-1) * 2n!/2!(2n-2)! = F(n-1) * (2n-1)2n/2 = F(n-1) * 4n^2-2n/2
+     = F(n-1) * n(2n-1)
+*/
 class Solution {
     public int countOrders(int n) {
-        long cnt = 1;
-        for(int i=1;i<=n; i++){
-            cnt = (cnt * i * (2*i-1)) % 1000000007;
+        long ans = 1;
+        for(int i=2; i<=n; i++){
+            ans = (ans*i*(2*i-1)) % 1000000007;
         }
-        return (int)cnt;
+        return (int)ans;
     }
 }
-
-// finish at 2020/2/24 11:28
+//2020-03-16 17:02:54
