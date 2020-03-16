2ae8f2774af1ff6bcde7cbcd73a7b5c8892a22ad
Egan <1154610080@qq.com>
2020-03-16 16:37:57 +0800

    面试题14- I. 剪绳子(review)

--- 力扣/剑指Offer/面试题14- I. 剪绳子/Solution.java
+++ 力扣/剑指Offer/面试题14- I. 剪绳子/Solution.java
@@ -1,15 +1,15 @@
-/**
- * @author Egan
- * @date 2020/2/24 10:19
- **/
-
-// begin at 2020/2/24 10:38
+//2020-03-16 16:21:59
+/*
+9%3==0 3*3*3=3^3=27
+108%3==1 3*3*3*4=3^3*4=108
+11%3==2 3*3*3*2 = 3^3*2
+*/
 class Solution {
     public int cuttingRope(int n) {
         if(n<=3) return n-1;
-        if(n%3==0) return (int) Math.pow(3, n/3);
-        if(n%3==1) return (int) Math.pow(3, n/3-1) * 2 * 2;
-        return (int) (Math.pow(3, n/3) * 2);
+        if(n%3==0) return (int)Math.pow(3, n/3);
+        if(n%3==1) return (int)Math.pow(3, n/3-1) * 4;
+        return (int)Math.pow(3, n/3)*2;
     }
 }
-// finish at 2020/2/24 10:47
+//2020-03-16 16:36:15
