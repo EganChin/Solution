d5f9f7a51a6282248b4700fb153ef18400fb1eae
Egan <1154610080@qq.com>
2020-03-18 16:18:28 +0800

    131. 分割回文串(review)

--- 力扣/算法/131. 分割回文串.java
+++ 力扣/算法/131. 分割回文串.java
@@ -1,42 +1,37 @@
-//begin at 2020-02-28 10:40:34
+//2020/3/18 16:06:37
 class Solution {
-    private String s;
-    private int len;
-    private List<List<String>> result = new ArrayList<>();
-    private Deque<String> path = new ArrayDeque<>();
+    String string;
+    Deque<String> path = new ArrayDeque<>();
+    List<List<String>> lists = new ArrayList<>();
 
     public List<List<String>> partition(String s) {
-        this.s = s;
-        this.len = s.length();
+        string = s;
         backtracking(0);
-        return result;
+        return lists;
     }
 
-    public void backtracking(int index){
-        if(index == len){
-            result.add(new ArrayList<>(path));
+    private void backtracking(int index){
+        if(index == string.length()){
+            lists.add(new ArrayList<>(path));
             return;
         }
-        for(int i=index; i<len; i++){
+        for(int i=index; i<string.length(); i++){
             if(isPalindrome(index, i)){
-                path.addLast(s.substring(index, i+1));
+                path.addLast(string.substring(index, i+1));
+                // System.out.println(string.substring(index, i+1));
                 backtracking(i+1);
                 path.removeLast();
             }
         }
     }
 
-    public boolean isPalindrome( int left, int right) {
-        while(left<right){
-            if(s.charAt(left) != s.charAt(right)){
+    private boolean isPalindrome(int left, int right){
+        while (left<right){
+            if(string.charAt(left++) != string.charAt(right--)){
                 return false;
-            }else{
-                left++;
-                right--;
             }
         }
         return true;
     }
 }
-
-//finish at 2020-02-28 10:57:15
+//2020-03-18 16:16:50
