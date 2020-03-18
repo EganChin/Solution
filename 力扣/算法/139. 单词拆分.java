2fe5585bd1380687931a56b4488e00f8c2e23ff1
Egan <1154610080@qq.com>
2020-03-18 16:31:29 +0800

    139. 单词拆分(review)

--- 力扣/算法/139. 单词拆分.java
+++ 力扣/算法/139. 单词拆分.java
@@ -1,28 +1,23 @@
-// begin at 2020-02-28 11:00:02
+//2020-03-18 16:20:22
 class Solution {
-    private Boolean []memo;
-
+    Boolean[] memo;
     public boolean wordBreak(String s, List<String> wordDict) {
         memo = new Boolean[s.length()+1];
-        return wordBreak(s, new HashSet<>(wordDict), 0);
+        return wordBreak(s, wordDict, 0);
     }
 
-    public boolean wordBreak(String s, Set<String> wordDict, int start) {
+    public boolean wordBreak(String s, List<String> wordDict, int start){
         if(start == s.length()){
             return memo[start] = true;
         }
-
-        if(memo[start] != null) return memo[start];
-
-        for(int end=start+1; end<=s.length(); end++){
+        if(memo[start]!=null) return memo[start];
+        for(int end = start; end<=s.length(); end++){
             if(wordDict.contains(s.substring(start, end)) && wordBreak(s, wordDict, end)){
-
-                return memo[start] = true;
+                return memo[end] = true;
             }
         }
-
         return memo[start] = false;
     }
 }
 
-//finish at 2020-02-28 11:23:12
+//2020-03-18 16:30:46
