41b751f7bf295ef1a3cb6957c2c2e9a65398de67
Egan <1154610080@qq.com>
2020-08-27 10:37:07 +0800

    131.分割回文串 re.1 ui redo: 1 time: 2020-08-27 10:05:13 - 10:34:26 i: No diff: m
    Q:给定一个字符串 s，将 s 分割成一些子串，使每个子串都是回文串。
    返回 s 所有可能的分割方案。
    
    A:这道题可以使用回溯和dfs的方法解决。
    首先以字符串的下标0位置作为起点，开始回溯。
    每次回溯以起点开始遍历整个字符串，如果从起点到当前位置的字符串是回文，
    那么将其加入到路径当中，然后以当前位置作为起点继续回溯，
    当起点位置超出字符串下标时，说明整个字符串已经分割完毕，这条路径可以完全分割为回文串，将其保存。

--- 力扣/算法/131. 分割回文串.java
+++ 力扣/算法/131. 分割回文串.java
@@ -1,37 +1,39 @@
-//2020/3/18 16:06:37
-class Solution {
-    String string;
-    Deque<String> path = new ArrayDeque<>();
-    List<List<String>> lists = new ArrayList<>();
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
 
+//2020-08-27 10:05:13
+class Solution {
+    private String s;
+    private Deque<String> path = new ArrayDeque<>();
+    private List<List<String>> lists = new ArrayList<>();
     public List<List<String>> partition(String s) {
-        string = s;
+        this.s = s;
         backtracking(0);
         return lists;
     }
 
     private void backtracking(int index){
-        if(index == string.length()){
+        if(index == s.length()){
             lists.add(new ArrayList<>(path));
             return;
         }
-        for(int i=index; i<string.length(); i++){
-            if(isPalindrome(index, i)){
-                path.addLast(string.substring(index, i+1));
-                // System.out.println(string.substring(index, i+1));
-                backtracking(i+1);
+        for(int right=index; right<s.length(); right++){
+            if(isPandrome(index, right)){
+                path.addLast(s.substring(index, right+1));
+                backtracking(right+1);
                 path.removeLast();
             }
         }
     }
 
-    private boolean isPalindrome(int left, int right){
-        while (left<right){
-            if(string.charAt(left++) != string.charAt(right--)){
+    private boolean isPandrome(int left, int right){
+        while(left < right){
+            if(s.charAt(left++) != s.charAt(right--)){
                 return false;
             }
         }
         return true;
     }
 }
-//2020-03-18 16:16:50
+//2020-08-27 10:34:26
